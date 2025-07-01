a846d0031562f09344f797d9ca47211bde4f9618
LavithGubba <lavithgubba@gmail.com>
2025-07-01 22:10:41 +0530

    Logo and Json changes

--- src/features/Partners/Partners.tsx
+++ src/features/Partners/Partners.tsx
@@ -8,6 +8,18 @@ import drreddy from "./../../assets/img/partners/drreddy.png";
 import granules from "./../../assets/img/partners/granules.png";
 import leepharma from "./../../assets/img/partners/leepharma.png";
 import nato from "./../../assets/img/partners/nato.png";
+import aragen from "./../../assets/img/partners/aragen.png";
+import csir from "./../../assets/img/partners/csir.png";
+import enveda from "./../../assets/img/partners/enveda.png";
+import honour from "./../../assets/img/partners/honour.png";
+import laurus from "./../../assets/img/partners/laurus.png";
+import msn from "./../../assets/img/partners/msn.png";
+import neuland from "./../../assets/img/partners/neuland.png";
+import nuwill from "./../../assets/img/partners/nuwill.png";
+import seasons from "./../../assets/img/partners/seasons.png";
+import ssv from "./../../assets/img/partners/ssv.png";
+import vasudha from "./../../assets/img/partners/vasudha.png";
+
 
 const Partners = () => {
   return (
@@ -49,6 +61,39 @@ const Partners = () => {
       </div>
       <div className="items-center flex justify-center">
           <img src={nato} alt="Natco Pharma" />
+      </div>
+      <div className="items-center flex justify-center">
+          <img src={aragen} alt="Aragen Life Sciences Ltd" />
+      </div>
+      <div className="items-center flex justify-center">
+          <img src={csir} alt="Central Institute of Medicinal and Aromatic Plants" />
+      </div>
+      <div className="items-center flex justify-center">
+          <img src={enveda} alt="Enveda" />
+      </div>
+      <div className="items-center flex justify-center">
+          <img src={honour} alt="Honour Lab Limited" />
+      </div>
+      <div className="items-center flex justify-center">
+          <img src={laurus} alt="Laurus labs" />
+      </div>
+      <div className="items-center flex justify-center">
+          <img src={msn} alt="MSN Laboratories" />
+      </div>
+      <div className="items-center flex justify-center">
+          <img src={neuland} alt="Neuland Labs" />
+      </div>
+      <div className="items-center flex justify-center">
+          <img src={nuwill} alt="Nuwill" />
+      </div>
+      <div className="items-center flex justify-center">
+          <img src={seasons} alt="Seasons" />
+      </div>
+      <div className="items-center flex justify-center">
+          <img src={ssv} alt="SSV" />
+      </div>
+      <div className="items-center flex justify-center">
+          <img src={vasudha} alt="Vasudha" />
       </div>
     </OwlCarousel>
   );

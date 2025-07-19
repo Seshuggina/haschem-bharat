108cfa9bbae5369a1ee45a1cc89b34f626ca365d
Seshu Vuggina <seshuggina@gmail.com>
2025-07-19 17:37:47 +0530

    Add ScrollToTop component to reset scroll position on route change

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,22 +6,27 @@ import { Services } from "../pages/Services/Services";
 // import { PageNotFound } from "../pages/PageNotFound/PageNotFound";
 import { Products } from "../pages/Products/Products";
 import { ProductDetails } from "../pages/ProductsDetails/ProductsDetails";
+import ScrollToTop from "../components/common/ScrollToTop";
+
 // import other pages as needed
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about-us" element={<AboutUs />} />
-      <Route path="/contact-us" element={<ContactUs />} />
-      <Route path="/services" element={<Services />} />
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/services" element={<Services />} />
 
-      <Route path="/products" element={<Products />} />
-      <Route path="/products-details/:id" element={<ProductDetails />} />
-      {/* <Route path="/career" element={<Career />} /> */}
+        <Route path="/products" element={<Products />} />
+        <Route path="/products-details/:id" element={<ProductDetails />} />
+        {/* <Route path="/career" element={<Career />} /> */}
 
-      <Route path="*" element={<Home />} />
-    </Routes>
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </>
   );
 };
 
